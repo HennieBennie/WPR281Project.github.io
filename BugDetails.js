6086a2cd2d0a89ce6a773d6dcb5f604adcb37b2a
Hennie <henkovermeulen746@gmail.com>
2026-04-18 10:45:14 +0200

    Bug details page and details js

---
+++ BugDetails.js
@@ -0,0 +1,21 @@
+let issues = JSON.parse(localStorage.getItem("issues")) || [];
+
+let link = new URLSearchParams(window.location.search);
+let id = link.get("id");
+
+let bug = issues.find(bug => bug.id === id);
+
+let container = document.getElementById("bugDetails");
+
+if(bug) {  
+    container.innerHTML = `
+    <p>Summary:${bug.summary}</p>
+    <p>Description:${bug.description}</p>
+    <p>Identified by:${bug.identifiedBy}</p>
+    <p>Assigned to:${bug.assignedTo}</p>
+    <p>Project:${bug.project}</p>
+    <p>Status:${bug.status}</p>
+    <p>Target date:${bug.targetDate}</p>
+    `
+
+}
--- scripts.js
+++ scripts.js
@@ -53,7 +53,7 @@ function displayBugsSum() {
 };
 //Display ALL of ticket
 function displayDetail(id) {
-  window.location.href = `details.html?id=${id}`;  
+  window.location.href = `BugDetails.html?id=${id}`;  
 };
 //save to ticket
 function saveEdit() {};
